3adafd4c948c5e03061711d7ca13fc5c9302b499
wanderso <williamharrisanderson@gmail.com>
2016-08-17 05:39:58 -0700

    Corrected file errors

--- test_data_producer.py
+++ test_data_producer.py
@@ -1,5 +1,14 @@
+test_data = []
+learn_data = []
+learn_odds = 0.9
+
 with file("./res/clean_package_names.txt", "r") as f:
-    read_line = f.readline()
-    while read_line is not "":
-        print read_line.strip("\n")
-        read_line = f.readline()
+    for line in f:
+        if len(line.strip()) is not 0 and line.strip()[0] is not "#":
+            learn_data.append(line)
+
+with file("./imp/package_no_comments.txt", "w") as f:
+    for entry in learn_data:
+        f.write(entry)
+
+
